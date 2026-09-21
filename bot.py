41011a6c64fafedb8370c73ef2d710afd5ed514f
AM-SAVIOUR <princesaviour6969@gmail.com>
2026-09-21 21:29:30 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -535,8 +535,7 @@ def send_lrc(uid, chat_id, query):
     file_bytes = io.BytesIO(lrc.encode("utf-8"))
     file_bytes.name = filename
     bot.send_document(chat_id, file_bytes,
-        caption=f"🎤 {song['trackName']} — {song['artistName']}")
-      bump(uid, "lyrics")
+        caption=f"🎤 {song['trackName']} — {song['artistName']}") bump(uid, "lyrics")
 
 # ---------- BUSINESS ----------
 BUSINESS_OWNERS = {}
